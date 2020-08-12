776cbf0d53645c4a2ae50a7f59569a1dbc16d145
강상원 <kevink1113@hanmail.net>
2020-08-12 10:13:49 +0900

    Initial Commit

---
+++ SeoulWeather_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeoulWeather_SwiftUI
+//
+//  Created by 강상원 on 2020/08/12.
+//  Copyright © 2020 강상원. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeoulWeather_SwiftUITests/SeoulWeather_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  SeoulWeather_SwiftUITests.swift
+//  SeoulWeather_SwiftUITests
+//
+//  Created by 강상원 on 2020/08/12.
+//  Copyright © 2020 강상원. All rights reserved.
+//
+
+import XCTest
+@testable import SeoulWeather_SwiftUI
+
+class SeoulWeather_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
